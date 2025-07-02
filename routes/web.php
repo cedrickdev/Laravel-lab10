0e22b114a74c56624166a85f34c75b3c317e5a19
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-07-02 20:10:53 +0200

    feat(route): add blog routes with slug and id parameters
    Added routes for the blog section, including a route to display individual blog posts based on slug and id, as well as an index route that generates a link to a specific blog post.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/blog')->name('blog.')->group(function () {
+
+    Route::get('/{slug}-{id}', function (string $slug, string $id, Request $request) {
+        return [
+            "slug" => $slug,
+            "id" => $id
+        ];
+    })->where([
+                "id" => '[0-9]+',
+                "slug" => '[a-z0-9]\-'
+            ])->name('show');
+
+    Route::get('/', function (Request $request) {
+        return [
+            "link" => \route('blog.show', ['slug' => 'article', 'id' => 13]),
+        ];
+    })->name('index');
+
 });
