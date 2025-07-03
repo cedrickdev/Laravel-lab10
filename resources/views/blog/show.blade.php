89ca04292147a750f9fe05394c7301f81dfc4e3e
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-07-03 19:53:19 +0200

    feat(view): add button to create a new blog post
    This commit adds a button to the blog show view, allowing users to easily navigate to the blog post creation page. This enhancement improves user experience by providing direct access to post creation from the individual post view.

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -5,6 +5,9 @@
 
 @section('content')
 
+    <p>
+               <a href="{{ route('post.edit', $post) }}" class="btn btn-warning"> Creer un blog </a>
+        </p>
 
         <h1>{{ $post->title }}</h1>
         <article>
