13b09b2e60a9fd3f9e62dc6e3b557c04ed568c8e
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-07-03 19:54:25 +0200

    refractor(controller): enhance PostController with validation and improved methods
    This commit updates the PostController to utilize the PostFormValidationRequest for creating and updating blog posts, ensuring that the data submitted meets validation criteria. The method signatures for show, update, and delete have been modified to improve type safety by using PostModel instead of raw IDs. Additionally, the create method now returns the view for creating posts, enhancing the overall structure and user experience of the blog management functionality.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreatePostRequest;
+use App\Http\Requests\PostFormValidationRequest;
 use App\Models\PostModel;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use \Illuminate\View\View;
+use \Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -22,61 +25,51 @@ class PostController extends Controller
     {
         $post = $this->post::paginate(1);
 
-        return view('blog.index',[
+        return view('blog.index', [
             'posts' => $post
         ]);
-
     }
 
-    public function show(string $slug, int $id): RedirectResponse | View
+    public function show(string $slug, PostModel $post): RedirectResponse | View
     {
-        $post = $this->post->findOrFail($id);
-        if($post->slug !== $slug){
+        $post = $this->post->findOrFail($post);
+        if ($post->slug !== $slug) {
             return to_route('post.show', ['slug' => $post->slug, 'id' => $post->id]);
         }
         return view('blog.show', [
             'post' => $post
-        ]); 
+        ]);
     }
 
     public function create()
     {
-        $createdPost = $this->post->create([
-            'title' => 'My fouth content',
-            'slug' => 'my-fouth-content',
-            'content' => 'Hello this is my fouth content :)'
-        ]);
-
-        echo "Post created successfully ";
-        return $createdPost;
-
+       
+        return view('blog.create');
     }
 
-    public function update(int $id)
+    public function store(PostFormValidationRequest $request)
     {
-
-        $updatedPost = $this->post->find($id);
-        if (!$updatedPost) {
-            echo "Sorry! post not found :( ";
-        }
-        $updatedPost->update([
-            'title' => 'My fouth content',
-            'slug' => 'my-fouth-content',
-            'content' => 'Hello this is my fouth content :)'
-        ]);
-        echo "Post updated successfully ";
-        return $updatedPost;
+        $createdPost = $this->post->create($request->validated());
+        return redirect()->route('post.show', ['slug' => $createdPost->slug, 'id' => $createdPost->id])->with('success', 'le Blog a éte crée avec succes'); //with() property is then passed in the session which is captured in the view template to dosplay the alert
     }
 
-    public function delete(int $id)
+    public function edit(PostModel $post ) :View{
+        return view('blog.edit', [
+         'post' => $post
+        ]);
+    }
+
+    public function update(PostModel $post, PostFormValidationRequest $request)
     {
-        $deletedPost = $this->post->find($id);
-        if (!$deletedPost) {
-            echo "Sorry! post not found :( ";
-        }
-        $deletedPost->update([
+        $post->update($request->validated());
+        return redirect()->route('post.show', ['slug' => $post->slug, 'id' => $post->id])->with('success', 'le Blog a éte modifié avec succes');
+    }
+
+    public function delete(PostModel $post)
+    {
+     
+        $post->update([
             'status' => 'inactive'
         ]);
-        echo "Post deleted sucessfully :)";
     }
 }
