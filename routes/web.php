4351d7bd34fca0768eeaf5b44e9b5c2ee688234e
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-07-03 19:54:15 +0200

    refractor(route): update blog routes for post management
    This commit updates the routing structure for blog posts by modifying the route parameters and adding new routes for creating, editing, and storing posts. The changes enhance the RESTful practices of the application and improve the organization of post-related actions.

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,10 @@ Route::get('/', function () {
 
 Route::controller(PostController::class)->prefix('post')->name('post.')->group(function () {
     Route::get('/', 'index')->name('index');
-    Route::get('/{slug}-{id}', 'show')->where(["id" => '[0-9]+', "slug" => '[a-z0-9\-]+'])->name('show');
-    Route::post('/create', 'create')->name('create');
+    Route::get('/{slug}-{post}', 'show')->where(["post" => '[0-9]+', "slug" => '[a-z0-9\-]+'])->name('show');
+    Route::get('/create', 'create')->name('create');
+    Route::post('/create', 'store')->name('store');
+    Route::get('/{post}/edit', 'edit')->name('edit');
     Route::post('/update/{id}', 'update')->name('update'); //post.update($id)
-    Route::post('/delete/{id}', 'delete')->name('delete'); //post.delete($id)
+    Route::post('/delete/{post}', 'delete')->name('delete'); //post.delete($id)
 });
