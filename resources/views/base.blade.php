5f75307c677e375502519cd3ec0f8aa8465bdaea
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-07-03 15:16:11 +0200

    feat(view): add base layout for blog with Bootstrap 5 integration
    This commit introduces a new base layout view for the blog, utilizing Bootstrap 5 for styling. The layout includes a responsive navigation bar and a container for dynamic content, enhancing the overall user interface and experience. Additionally, the AppServiceProvider is updated to use Bootstrap 5 pagination, ensuring consistent styling across paginated views.

---
+++ resources/views/base.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <style>
+        @layer demo {
+            button {
+                all: unset;
+
+            }
+        }
+    </style>
+
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Blog</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="{{ route('post.index') }}">Acceuil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+    
+    </div>
+  </div>
+</nav>
+
+     <div class="container">
+         @yield('content')
+     </div>
+</body>
+</html>
